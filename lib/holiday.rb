03ead6e1e28d02e97848a8db594dee117dd423c8
Zoey Chesny <z.chesny@gmail.com>
2020-04-13 19:09:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,5 +69,5 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season, supply_hash|
-    
+
 end
